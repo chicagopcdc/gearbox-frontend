019ac9be3b10ba1e64a60ec74830bb08785be7e0
bobaekang <bkang.dev@gmail.com>
2020-05-20 11:00:39 -0500

    Setup basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Guide from './Pages/Guide'
+import Login from './Pages/Login'
+import Results from './Pages/Results'
+import Trials from './Pages/Trials'
+
+const routes = [
+  { component: Home, name: 'Home', path: '/', exact: true },
+  { component: About, name: 'About', path: '/about' },
+  { component: Guide, name: 'Guide', path: '/guide' },
+  { component: Login, name: 'Login', path: '/login' },
+  { component: Results, name: 'Results', path: '/results' },
+  { component: Trials, name: 'Trials', path: '/trials' },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            {routes.map(({ path, name }) => (
+              <li>
+                <Link to={path}>{name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <Switch>
+          {routes.map(({ exact, path, component }) => (
+            <Route exact={exact} path={path}>
+              {component}
+            </Route>
+          ))}
+        </Switch>
+      </main>
+    </Router>
   )
 }
 
