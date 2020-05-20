148c2e48877e0df06c2abb4719b0664f78e6c184
bobaekang <bkang.dev@gmail.com>
2020-05-20 13:26:09 -0500

    Update nav item names

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ const routes = [
   { component: Home, name: 'Home', path: '/', exact: true, nav: false },
   { component: Results, name: 'Results', path: '/results', nav: false },
   { component: Login, name: 'Login', path: '/login', nav: false },
-  { component: Guide, name: 'Guide', path: '/guide', nav: true },
-  { component: Trials, name: 'Trials', path: '/trials', nav: true },
-  { component: About, name: 'About', path: '/about', nav: true },
+  { component: Guide, name: 'Guide for Use', path: '/guide', nav: true },
+  { component: Trials, name: 'Eligible Trials', path: '/trials', nav: true },
+  { component: About, name: 'About GEARBOx', path: '/about', nav: true },
 ]
 
 function App() {
