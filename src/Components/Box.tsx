19ba302c90f53ecd5f3888e49557ee17c1287efd
bobaekang <bkang.dev@gmail.com>
2020-05-22 14:12:17 -0500

    Create Box component for a stylized container

---
+++ src/Components/Box.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const styles = {
+  box: 'border-2 border-solid border-black m-4',
+  boxNameOuter: 'text-base -mt-4 ml-2 mb-0',
+  boxNameInner: 'bg-white px-2',
+  boxContent: 'p-4',
+}
+
+type BoxProps = {
+  name: string
+  children: React.ReactNode
+}
+
+const Box = ({ name, children }: BoxProps) => {
+  return (
+    <div className={styles.box}>
+      <h1 className={styles.boxNameOuter}>
+        <span className={styles.boxNameInner}>{name}</span>
+      </h1>
+      <div className={styles.boxContent}>{children}</div>
+    </div>
+  )
+}
+
+export default Box
