1a4274c794020ed0d1a6d0db5fcbdfb22f1bff24
bobaekang <bkang.dev@gmail.com>
2020-05-26 14:41:13 -0500

    Add optional extra element to display on Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import pcdcLogo from '../assets/pcdc-logo.png'
 
 const styles = {
   footerContent:
     'flex flex-wrap items-center justify-between border-t-2 border-solid border-black mx-4 py-4',
 }
 
-const Footer = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styles.footerContent}>{children}</div>
+const ExtraFooter = () => (
+  <div className={styles.footerContent}>
+    <p>
+      New to the Pediatric Cancer Data Commons? Visit our website for more
+      details on GEARBOx.
+    </p>
+    <img src={pcdcLogo} alt="logo" style={{ height: '100px' }} />
+  </div>
+)
+
+type FooterProps = {
+  children: React.ReactNode
+  showExtra: boolean
+}
+
+const Footer = ({ children, showExtra = false }: FooterProps) => {
+  return (
+    <>
+      {showExtra && <ExtraFooter />}
+      <div className={styles.footerContent}>{children}</div>
+    </>
+  )
 }
 
 export default Footer
