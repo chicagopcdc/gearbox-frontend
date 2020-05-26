ec5d170a810ca9e1b739f513bc4fbcd55b1efb60
bobaekang <bkang.dev@gmail.com>
2020-05-26 12:24:32 -0500

    Set up basic About page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Box from '../Components/Box'
+
+const steps = [
+  `Study coordinators gather patient information, lab values, flow cytometry, and cytogenetic reports.`,
+  `Specific pieces of a patient's clinical information are submitted to the online portal.`,
+  `The GEARBOx algorithm filters the patient's information through the eligibility criteria for each pediatric AML trial.`,
+  `Study coordinators are given a list of trials for which their patient is eligible, including registration information and contact information for the managing institution`,
+]
 
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <Box name="GEARBOx Overview">
+      <div className="mx-8">
+        <ol className="list-decimal mt-8 mb-16">
+          {steps.map((step) => (
+            <li className="mb-4">{step}</li>
+          ))}
+        </ol>
+
+        <img alt="Diagram" />
+      </div>
+    </Box>
+  )
 }
 
 export default About
