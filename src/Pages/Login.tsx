134a486d1b3e8260424314f9bb1df2c21798c23b
bobaekang <bkang.dev@gmail.com>
2020-05-26 14:33:43 -0500

    Set up basic Login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Box from '../Components/Box'
+import Button from '../Components/Button'
+import LoginForm from '../Components/LoginForm'
+
+import gearboxLogo from '../assets/gearbox-logo.png'
 
 const Login = () => {
-  return <h1>Login</h1>
+  return (
+    <div className="flex flex-col h-full align-center">
+      <div className="m-auto flex-col align-center justify-center">
+        <img src={gearboxLogo} alt="logo" style={{ height: '150px' }} />
+
+        <Box name="Login">
+          <LoginForm />
+        </Box>
+
+        <div className="flex justify-around">
+          <Button onclick={() => alert('add user')}>Add User</Button>
+          <Button onclick={() => alert('add institution')}>
+            Add Institution
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
