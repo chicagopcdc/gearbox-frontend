1cfa2d24f59e5fd15e2e8c669a59cbea982e938a
bobaekang <bkang.dev@gmail.com>
2020-06-01 11:25:10 -0500

    Create a simple MultiSelect component

---
+++ src/Components/Inputs/MultiSelect.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import ReactMultiSelect from 'react-multi-select-component'
+
+type FormikMinimalTarget = { name: string; value: string[] }
+
+type MultiSelectProps = {
+  label?: string
+  name?: string
+  options: string[]
+  disabled?: boolean
+  placeholder?: string
+  values?: string[]
+  onChange?({ target }: { target: FormikMinimalTarget }): void
+}
+
+const reshapeToMulti = (options: string[]) =>
+  options.map((option) => ({
+    label: option,
+    value: option,
+  }))
+
+const MultiSelect = ({
+  label,
+  name,
+  options,
+  values,
+  disabled,
+  placeholder,
+  onChange,
+}: MultiSelectProps) => {
+  const multiOptions = reshapeToMulti(options)
+  const [multiSelected, setMultiSelected] = useState(
+    reshapeToMulti(values || [])
+  )
+
+  useEffect(() => {
+    if (onChange && name) {
+      onChange({
+        target: {
+          name,
+          value: multiSelected.map((selected) => selected.value),
+        },
+      })
+    }
+  }, [multiSelected, name, onChange])
+
+  return (
+    <ReactMultiSelect
+      options={multiOptions}
+      value={multiSelected}
+      onChange={setMultiSelected}
+      disabled={disabled}
+      labelledBy={label || ''}
+      overrideStrings={{
+        selectSomeItems: placeholder || '',
+      }}
+      hasSelectAll={false}
+    />
+  )
+}
+
+export default MultiSelect
