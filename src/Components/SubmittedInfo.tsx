94f7da9e3a60230489fdbe784aba2e9e4e23336c
bobaekang <bkang.dev@gmail.com>
2020-06-10 10:33:57 -0500

    Move labels and biomarkers consts to config file

--- src/Components/SubmittedInfo.tsx
+++ src/Components/SubmittedInfo.tsx
@@ -4,42 +4,7 @@ import Checkbox from './Inputs/Checkbox'
 import TextField from './Inputs/TextField'
 import Textarea from './Inputs/Textarea'
 
-const labels: {
-  treatments: { [key: string]: string }
-  organFunction: { [key: string]: string }
-} = {
-  treatments: {
-    prevChemoFlag: 'previous chemotherapy',
-    prevRadFlag: 'previous radiation therapy',
-    prevAtra: 'all-trans retinoid acid (ATRA)',
-    prevHydroxyurea: 'hydroxyurea',
-    prevSteroids: 'corticosteriods',
-    prevItCyt: 'IT cytarabine',
-    prevOther: 'other antileukemic therapy',
-  },
-  organFunction: {
-    lvEf: 'Left Ventricular Ejection Fraction (%)',
-    secrumCr: 'Baseline serum creatinine (mg/dL)',
-    astRecent: 'Most recent AST (U/L)',
-    altRecent: 'Most recent ALT (U/L)',
-  },
-}
-
-const allBiomarkers = [
-  'MECOM(3q26.2)',
-  't(6;9)(p23;q34.1)(DEK-NUP214)',
-  'Monosomy 7',
-  'Monosomy 5/5q-[ERG1(5q31) deleted]',
-  'KMT2A(MLL)(11q23.3)',
-  'NUP98(11p15.5)',
-  '12p abnormalities (ETV6)',
-  'ETS FUS-ERG Fusion',
-  'FLT3/ITD+ with alleic ratio > 0.1%',
-  'CBFA2T3-GLIS2',
-  'RAM phenotype',
-  'KAT6A(8p11.21) Fusion',
-  'Non-KMT2A MLLT10 Fusions',
-]
+import { biomarkers as allBiomarkers, labels } from '../config'
 
 type SubmittedInfoProps = {
   data: {
