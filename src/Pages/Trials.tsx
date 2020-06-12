d94cbcb6be5a2382009d59caf61d90b2780297bd
bobaekang <bkang.dev@gmail.com>
2020-06-12 09:35:51 -0500

    Use TrialCard in Trials page

--- src/Pages/Trials.tsx
+++ src/Pages/Trials.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Box from '../Components/Box'
+import TrialCard, { Trial } from '../Components/TrialCard'
 
-const Trials = () => {
-  return <h1>Trials</h1>
-}
+const Trials = ({ data }: { data?: Trial[] }) => (
+  <Box name="Complete List of Trials">
+    <div className="flex flex-wrap justify-center">
+      {(data || []).map((trial) => (
+        <TrialCard data={trial}></TrialCard>
+      ))}
+    </div>
+  </Box>
+)
 
 export default Trials
