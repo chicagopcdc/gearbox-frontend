09cc0c209ffb391137295eb4a55ea9a1b976d197
bobaekang <bkang.dev@gmail.com>
2020-06-29 14:48:19 -0500

    Make TrialCard more compact

--- src/Components/TrialCard.tsx
+++ src/Components/TrialCard.tsx
@@ -4,7 +4,7 @@ const styles = {
   container:
     'border border-solid border-black hover:border-red-500 hover:bg-red-100 m-4 py-4 px-8',
   title: 'font-bold text-lg pb-2',
-  subtitle: 'font-bold py-2',
+  subtitle: 'font-bold inline pr-2',
 }
 
 export type Trial = {
@@ -23,17 +23,23 @@ const TrialCard = ({
     <h2 className={styles.title}>{title}</h2>
 
     <h3 className={styles.subtitle}>Research group</h3>
-    <p>{group}</p>
+    {group}
+    <br />
 
     <h3 className={styles.subtitle}>Location</h3>
-    <p>{location}</p>
+    {location}
+    <br />
 
-    <h3 className={styles.subtitle}>Registration information</h3>
-    {registerLinks?.map(({ name, url }, i) => (
-      <a className="block text-blue-700" href={url} target="blank" key={i}>
-        {name}
-      </a>
-    ))}
+    <details>
+      <summary>
+        <h3 className={styles.subtitle}>Registration information</h3>
+      </summary>
+      {registerLinks?.map(({ name, url }, i) => (
+        <a className="block text-blue-700" href={url} target="blank" key={i}>
+          {name}
+        </a>
+      ))}
+    </details>
   </div>
 )
 
