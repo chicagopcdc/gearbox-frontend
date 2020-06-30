87648f745463403a967b1d7ba057a07d682ba166
bobaekang <bkang.dev@gmail.com>
2020-06-30 11:17:04 -0500

    Use innerClassName for Box children styles

--- src/Pages/Trials.tsx
+++ src/Pages/Trials.tsx
@@ -3,12 +3,13 @@ import Box from '../Components/Box'
 import TrialCard, { Trial } from '../Components/TrialCard'
 
 const Trials = ({ data }: { data?: Trial[] }) => (
-  <Box name="Complete List of Trials">
-    <div className="flex flex-wrap justify-center">
-      {(data || []).map((trial, i) => (
-        <TrialCard data={trial} key={i}></TrialCard>
-      ))}
-    </div>
+  <Box
+    name="Complete List of Trials"
+    innerClassName="flex flex-wrap justify-center"
+  >
+    {(data || []).map((trial, i) => (
+      <TrialCard data={trial} key={i}></TrialCard>
+    ))}
   </Box>
 )
 
