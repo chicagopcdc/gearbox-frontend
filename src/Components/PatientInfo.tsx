d199b75ca5ffea0ff8a11394a0871ab3597a0811
bobaekang <bkang.dev@gmail.com>
2020-07-01 14:32:13 -0500

    Inline param destructuring

--- src/Components/PatientInfo.tsx
+++ src/Components/PatientInfo.tsx
@@ -21,74 +21,72 @@ type PatientInfoProps = {
   }
 }
 
-const PatientInfo = ({ data }: PatientInfoProps) => {
-  const {
+const PatientInfo = ({
+  data: {
     priorTreatmentTherapies,
     organFunction,
     prevChemo,
     prevRad,
     biomarkers,
-  } = data
-
-  return (
-    <>
-      <div className="md:flex md:flex-wrap justify-around">
-        <div className={styles.group}>
-          <h2 className={styles.groupName}>Prior treatment therapies</h2>
-          {Object.keys(priorTreatmentTherapies).map((key) => (
-            <div className={styles.field} key={key}>
-              <Checkbox
-                label={labels[key]}
-                checked={priorTreatmentTherapies[key]}
-                readOnly
-              />
-            </div>
-          ))}
-        </div>
-
-        <div className={styles.group}>
-          <h2 className={styles.groupName}>Organ Function</h2>
-          {Object.keys(organFunction).map((key) => (
-            <div className={styles.field} key={key}>
-              <TextField
-                label={labels[key]}
-                value={organFunction[key]}
-                type="number"
-                min={0}
-                max={999}
-                readOnly
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="md:flex md:flex-wrap justify-around">
-        <div className={styles.group}>
-          <h2 className={styles.groupName}>Prior chemotherapy</h2>
-          <Textarea value={prevChemo.join('\n')} readOnly />
-        </div>
-
-        <div className={styles.group}>
-          <h2 className={styles.groupName}>Prior radiation therapy</h2>
-          <Textarea value={prevRad.join('\n')} readOnly />
-        </div>
-      </div>
-
+  },
+}: PatientInfoProps) => (
+  <>
+    <div className="md:flex md:flex-wrap justify-around">
       <div className={styles.group}>
-        <h2 className={styles.groupName}>Biomarkers</h2>
-        {allBiomarkers.map((marker) => (
-          <div className={styles.field} key={marker}>
+        <h2 className={styles.groupName}>Prior treatment therapies</h2>
+        {Object.keys(priorTreatmentTherapies).map((key) => (
+          <div className={styles.field} key={key}>
             <Checkbox
-              label={marker}
-              checked={biomarkers.includes(marker)}
+              label={labels[key]}
+              checked={priorTreatmentTherapies[key]}
               readOnly
             />
           </div>
         ))}
       </div>
-    </>
-  )
-}
+
+      <div className={styles.group}>
+        <h2 className={styles.groupName}>Organ Function</h2>
+        {Object.keys(organFunction).map((key) => (
+          <div className={styles.field} key={key}>
+            <TextField
+              label={labels[key]}
+              value={organFunction[key]}
+              type="number"
+              min={0}
+              max={999}
+              readOnly
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="md:flex md:flex-wrap justify-around">
+      <div className={styles.group}>
+        <h2 className={styles.groupName}>Prior chemotherapy</h2>
+        <Textarea value={prevChemo.join('\n')} readOnly />
+      </div>
+
+      <div className={styles.group}>
+        <h2 className={styles.groupName}>Prior radiation therapy</h2>
+        <Textarea value={prevRad.join('\n')} readOnly />
+      </div>
+    </div>
+
+    <div className={styles.group}>
+      <h2 className={styles.groupName}>Biomarkers</h2>
+      {allBiomarkers.map((marker) => (
+        <div className={styles.field} key={marker}>
+          <Checkbox
+            label={marker}
+            checked={biomarkers.includes(marker)}
+            readOnly
+          />
+        </div>
+      ))}
+    </div>
+  </>
+)
 
 export default PatientInfo
