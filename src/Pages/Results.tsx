5576d44b932cdc4abe3955c7011e6f4232bda1d3
bobaekang <bkang.dev@gmail.com>
2020-07-01 14:30:19 -0500

    Inline type definition and param destructuring

--- src/Pages/Results.tsx
+++ src/Pages/Results.tsx
@@ -3,44 +3,39 @@ import Box from '../Components/Box'
 import PatientInfo from '../Components/PatientInfo'
 import TrialCard, { Trial } from '../Components/TrialCard'
 
-type ResultsData = {
-  information: {
-    priorTreatmentTherapies: { [key: string]: boolean }
-    organFunction: { [key: string]: number }
-    prevChemo: string[]
-    prevRad: string[]
-    biomarkers: string[]
-  }
-  results: {
-    isLoaded: boolean
-    isError: boolean
-    trials: Trial[]
-  }
-}
-
 type ResultsProps = {
-  data: ResultsData
+  data: {
+    information: {
+      priorTreatmentTherapies: { [key: string]: boolean }
+      organFunction: { [key: string]: number }
+      prevChemo: string[]
+      prevRad: string[]
+      biomarkers: string[]
+    }
+    results: {
+      isLoaded: boolean
+      isError: boolean
+      trials: Trial[]
+    }
+  }
 }
 
-const Results = ({ data }: ResultsProps) => {
-  const { information, results } = data
-  return (
-    <>
-      <Box name="Summary of Submitted Patient Information">
-        <PatientInfo data={information} />
-      </Box>
+const Results = ({ data: { information, results } }: ResultsProps) => (
+  <>
+    <Box name="Summary of Submitted Patient Information">
+      <PatientInfo data={information} />
+    </Box>
 
-      <Box name="Results">
-        <div className="flex flex-wrap justify-center">
-          {results.isLoaded
-            ? results.trials.map((trial, i) => (
-                <TrialCard data={trial} key={i}></TrialCard>
-              ))
-            : 'Loading matched trials...'}
-        </div>
-      </Box>
-    </>
-  )
-}
+    <Box name="Results">
+      <div className="flex flex-wrap justify-center">
+        {results.isLoaded
+          ? results.trials.map((trial, i) => (
+              <TrialCard data={trial} key={i}></TrialCard>
+            ))
+          : 'Loading matched trials...'}
+      </div>
+    </Box>
+  </>
+)
 
 export default Results
