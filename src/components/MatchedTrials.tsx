a28938b26581109d718d4ac229d9d41110a945e4
bobaekang <bkang.dev@gmail.com>
2020-07-09 14:22:55 -0500

    Display both matched and unmatched trials

--- src/components/MatchedTrials.tsx
+++ src/components/MatchedTrials.tsx
@@ -9,15 +9,22 @@ type MatchedTrialsProps = {
   trials: Trial[]
 }
 
-const MatchedTrials = ({ className, matchIds, trials }: MatchedTrialsProps) => {
-  const matches = trials.filter(({ id }) => matchIds.includes(id))
-  return (
-    <Box name="Matched Trials" outerClassName={className}>
-      {matches.length > 0
-        ? matches.map((trial, i) => <TrialCard data={trial} key={i} />)
-        : 'No matches'}
-    </Box>
-  )
-}
+const MatchedTrials = ({ className, matchIds, trials }: MatchedTrialsProps) => (
+  <Box name="Matched Trials" outerClassName={className}>
+    <h2 className="font-bold text-center">Matched</h2>
+    {trials
+      .filter(({ id }) => matchIds.includes(id))
+      .map((trial, i) => (
+        <TrialCard data={trial} key={i} />
+      ))}
+    <br />
+    <h2 className="font-bold text-center">Unmatched</h2>
+    {trials
+      .filter(({ id }) => !matchIds.includes(id))
+      .map((trial, i) => (
+        <TrialCard data={trial} key={i} />
+      ))}
+  </Box>
+)
 
 export default MatchedTrials
