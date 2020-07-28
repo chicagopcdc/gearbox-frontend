f11b839c74c0fed5a7e0ad9570b7510766874357
Bobae Kang <bkang.dev@gmail.com>
2020-07-28 11:32:37 -0500

    Add small prop to Button component

--- src/components/Inputs/Button.tsx
+++ src/components/Inputs/Button.tsx
@@ -3,22 +3,23 @@ import React from 'react'
 type ButtonProps = {
   children: React.ReactNode
   disabled?: boolean
+  small?: boolean
   type?: 'button' | 'submit' | 'reset' | undefined
   onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 const styles = {
-  button(disabled?: boolean) {
-    return `border border-solid rounded px-4 py-2 ${
+  button(disabled?: boolean, small?: boolean) {
+    return `border border-solid rounded px-4 ${
       disabled
         ? 'cursor-not-allowed opacity-50'
         : 'border-black hover:bg-gray-300'
-    }`
+    } ${small ? 'text-xs py-1' : 'py-2'}`
   },
 }
 
-const Button = ({ children, disabled, ...attrs }: ButtonProps) => (
-  <button {...attrs} className={styles.button(disabled)}>
+const Button = ({ children, disabled, small, ...attrs }: ButtonProps) => (
+  <button {...attrs} className={styles.button(disabled, small)}>
     {children}
   </button>
 )
