442930eec819adf453c9ef600076e86bb5563c49
Bobae Kang <bkang.dev@gmail.com>
2020-08-04 14:02:05 -0500

    Remove obsolote comment

--- src/App.tsx
+++ src/App.tsx
@@ -72,8 +72,6 @@ function App() {
   const handleMatchFormChange = (values: MatchFormValues) => {
     if (JSON.stringify(matchFormValues) !== JSON.stringify(values)) {
       setMatchFormValues({ ...values })
-
-      // reset match result
       setIsMatchUpdating(true)
       setTimeout(() => {
         setMatchIds(getMatchIds(studies, values))
