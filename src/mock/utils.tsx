54113373c6e02609a190f1edf6e735374192c9c5
Bobae Kang <bkang.dev@gmail.com>
2020-08-05 15:10:56 -0500

    Prepare mocking data and util functions

---
+++ src/mock/utils.tsx
@@ -0,0 +1,9 @@
+import eligibilityCriteria from './eligibilityCriteria.json'
+import matchFormConfig from './matchFormConfig.json'
+import studies from './studies.json'
+
+export const loadMockEligibilityCriteria = () => eligibilityCriteria
+
+export const loadMockMatchFromConfig = () => matchFormConfig
+
+export const loadMockStudies = () => studies
