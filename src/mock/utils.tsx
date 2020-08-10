d3e8104d820f57c3b49a126d783df158f76627bb
Bobae Kang <bkang.dev@gmail.com>
2020-08-10 11:43:28 -0500

    Add util function to load mock latestUserInput

--- src/mock/utils.tsx
+++ src/mock/utils.tsx
@@ -1,10 +1,20 @@
 import eligibilityCriteria from './eligibilityCriteria.json'
+import latestUserInput from './latestUserInput.json'
 import matchConditions from './matchConditions.json'
 import matchFormConfig from './matchFormConfig.json'
 import studies from './studies.json'
+import { MatchFormValues } from '../model'
 
 export const loadMockEligibilityCriteria = () =>
   Promise.resolve(eligibilityCriteria)
+
+export const loadMockLatestUserInput = () =>
+  Promise.resolve(latestUserInput).then((fields) =>
+    fields.reduce(
+      (acc, { id, value }) => ({ ...acc, [id]: value }),
+      {} as MatchFormValues
+    )
+  )
 
 export const loadMockMatchConditions = () => Promise.resolve(matchConditions)
 
