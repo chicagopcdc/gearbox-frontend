4539983aca6d115de0bab486e3b08d9277331e50
Bobae Kang <bkang.dev@gmail.com>
2020-11-04 16:16:17 -0600

    Import font to use

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'fontsource-bebas-neue'
+import 'fontsource-lato/300.css'
+import 'fontsource-lato/400.css'
 import './index.css'
 import App from './App'
 
