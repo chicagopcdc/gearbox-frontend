2bad7f3364aef66702bc92bed7cee1492cfd78fe
Bobae Kang <bkang.dev@gmail.com>
2020-11-06 10:24:33 -0600

    Update Select style to put label on top

--- src/components/Inputs/Select.tsx
+++ src/components/Inputs/Select.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import Label from './Label'
 
 const styles = {
-  container: 'sm:flex sm:items-center',
-  label: 'sm:w-1/2 mr-4',
-  input:
-    'form-select rounded-none border border-solid border-black p-1 w-full sm:w-1/2',
+  container: 'flex flex-col',
+  input: 'form-select rounded-none border border-solid border-black p-1 w-full',
 }
 
 type SelectProps = {
@@ -28,9 +26,7 @@ const Select = ({
   ...attr
 }: SelectProps) => (
   <div className={styles.container}>
-    {label && (
-      <Label className={styles.label} text={label} htmlFor={name || ''} />
-    )}
+    {label && <Label text={label} htmlFor={name || ''} />}
     <select
       {...attr}
       className={styles.input}
