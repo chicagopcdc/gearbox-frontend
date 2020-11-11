486a5802b7cd258f3f7977247f434d4fb33f3826
Bobae Kang <bkang.dev@gmail.com>
2020-11-11 09:55:41 -0600

    Create DropdownSection component

---
+++ src/components/DropdownSection.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { ChevronUp, ChevronDown } from 'react-feather'
+
+type DropdownSectionProps = {
+  name: string
+  children: React.ReactNode
+}
+
+function DropdownSection({ name, children }: DropdownSectionProps) {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(true)
+  const handleOpen = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    setIsDropDownOpen(true)
+  }
+  const handleClose = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    setIsDropDownOpen(false)
+  }
+  return (
+    <section className="my-4">
+      <div className="flex py-2 justify-between border-b border-solid border-black">
+        <h2 className="font-bold">{name}</h2>
+        {isDropDownOpen ? (
+          <button onClick={handleClose}>
+            <ChevronUp color="#C00" />
+          </button>
+        ) : (
+          <button onClick={handleOpen}>
+            <ChevronDown />
+          </button>
+        )}
+      </div>
+      <div className={isDropDownOpen ? 'mx-2' : 'hidden'}>{children}</div>
+    </section>
+  )
+}
+
+export default DropdownSection
