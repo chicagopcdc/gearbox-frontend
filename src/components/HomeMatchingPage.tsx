9fec69005621667c79f4f507bb4ffff705be11c1
Bobae Kang <bkang.dev@gmail.com>
2021-03-03 15:42:24 -0600

    Adjust MatchForm/Result spacing

--- src/components/HomeMatchingPage.tsx
+++ src/components/HomeMatchingPage.tsx
@@ -19,16 +19,14 @@ function HomeMatchingPage({
 
   return isMatchDataReady ? (
     <div className="md:flex">
-      <div className="md:w-1/2 p-4 md:pr-8 lg:pr-12">
+      <div className="md:w-1/2 p-4 lg:px-8">
         <h1 className="uppercase text-primary font-bold">
           Patient Information
         </h1>
         <MatchForm {...matchFormProps} />
       </div>
       <div
-        className={`md:w-1/2 p-4 md:pl-8 lg:pl-12 ${
-          isChanging ? 'bg-gray-100' : ''
-        }`}
+        className={`md:w-1/2 p-4 lg:px-8 ${isChanging ? 'bg-gray-100' : ''}`}
       >
         <h1 className="uppercase text-primary font-bold">Open Trials</h1>
         <MatchResult {...matchResultProps} />
