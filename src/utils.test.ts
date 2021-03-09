385d96462281f11ff106a235ead47248c24be9b1
Bobae Kang <bkang.dev@gmail.com>
2021-03-09 15:16:00 -0600

    Scope tests by util function

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -49,140 +49,147 @@ const conditions: MatchCondition[] = [
     },
   },
 ]
-const getMatchGroupsTestHelper = (values: MatchFormValues) =>
-  getMatchGroups(getMatchDetails(criteria, conditions, config, values))
 
-test('getMatchGroups for simple AND algorithm', () => {
-  const values: MatchFormValues = {
-    0: true,
-    1: false,
-  }
-  expect(getMatchGroupsTestHelper(values)).toEqual({
-    matched: [0, 1],
-    partiallyMatched: [],
-    unmatched: [2],
+describe('getMatchGroups', () => {
+  const getMatchGroupsTestHelper = (values: MatchFormValues) =>
+    getMatchGroups(getMatchDetails(criteria, conditions, config, values))
+
+  test('for simple AND algorithm', () => {
+    const values: MatchFormValues = {
+      0: true,
+      1: false,
+    }
+    expect(getMatchGroupsTestHelper(values)).toEqual({
+      matched: [0, 1],
+      partiallyMatched: [],
+      unmatched: [2],
+    })
+  })
+
+  test('for simple OR algorithm', () => {
+    const values: MatchFormValues = {
+      0: false,
+      1: true,
+    }
+    expect(getMatchGroupsTestHelper(values)).toEqual({
+      matched: [1],
+      partiallyMatched: [],
+      unmatched: [0, 2],
+    })
+  })
+
+  test('for nested algorithm', () => {
+    const values: MatchFormValues = {
+      0: true,
+      2: 1,
+    }
+    expect(getMatchGroupsTestHelper(values)).toEqual({
+      matched: [1, 2],
+      partiallyMatched: [0],
+      unmatched: [],
+    })
   })
 })
 
-test('getMatchGroups for simple OR algorithm', () => {
-  const values: MatchFormValues = {
-    0: false,
-    1: true,
-  }
-  expect(getMatchGroupsTestHelper(values)).toEqual({
-    matched: [1],
-    partiallyMatched: [],
-    unmatched: [0, 2],
+describe('getIsFieldShowing', () => {
+  test('for one criterion (eq)', () => {
+    const showIf: MatchFormFieldShowIfCondition = {
+      operator: 'OR',
+      criteria: [{ id: 0, operator: 'eq', value: true }],
+    }
+    const values: MatchFormValues = { 0: true, 1: undefined, 2: undefined }
+
+    expect(getIsFieldShowing(showIf, config.fields, values)).toEqual(true)
+  })
+
+  test('for one criterion (gt)', () => {
+    const showIf: MatchFormFieldShowIfCondition = {
+      operator: 'OR',
+      criteria: [{ id: 2, operator: 'gt', value: 1 }],
+    }
+    const values: MatchFormValues = { 0: true, 1: undefined, 2: 2 }
+
+    expect(getIsFieldShowing(showIf, config.fields, values)).toEqual(true)
+  })
+
+  test('for multiple criteria (OR)', () => {
+    const showIf: MatchFormFieldShowIfCondition = {
+      operator: 'OR',
+      criteria: [
+        { id: 0, operator: 'eq', value: true },
+        { id: 1, operator: 'eq', value: true },
+        { id: 2, operator: 'gt', value: 1 },
+      ],
+    }
+    const values1: MatchFormValues = { 0: true }
+    expect(getIsFieldShowing(showIf, config.fields, values1)).toEqual(true)
+
+    const values2: MatchFormValues = { 0: false, 1: true }
+    expect(getIsFieldShowing(showIf, config.fields, values2)).toEqual(true)
+
+    const values3: MatchFormValues = { 0: false, 1: false, 2: 2 }
+    expect(getIsFieldShowing(showIf, config.fields, values3)).toEqual(true)
+
+    const values4: MatchFormValues = { 0: false, 1: false, 2: 1 }
+    expect(getIsFieldShowing(showIf, config.fields, values4)).toEqual(false)
+  })
+
+  test('for multiple criteria (AND)', () => {
+    const showIf: MatchFormFieldShowIfCondition = {
+      operator: 'AND',
+      criteria: [
+        { id: 0, operator: 'eq', value: true },
+        { id: 1, operator: 'eq', value: true },
+        { id: 2, operator: 'gt', value: 1 },
+      ],
+    }
+    const values1: MatchFormValues = { 0: true }
+    expect(getIsFieldShowing(showIf, config.fields, values1)).toEqual(false)
+
+    const values2: MatchFormValues = { 0: true, 1: false }
+    expect(getIsFieldShowing(showIf, config.fields, values2)).toEqual(false)
+
+    const values3: MatchFormValues = { 0: true, 1: true, 2: 2 }
+    expect(getIsFieldShowing(showIf, config.fields, values3)).toEqual(true)
+
+    const values4: MatchFormValues = { 0: true, 1: true, 2: 1 }
+    expect(getIsFieldShowing(showIf, config.fields, values4)).toEqual(false)
   })
 })
 
-test('getMatchGroups for nested algorithm', () => {
-  const values: MatchFormValues = {
-    0: true,
-    2: 1,
-  }
-  expect(getMatchGroupsTestHelper(values)).toEqual({
-    matched: [1, 2],
-    partiallyMatched: [0],
-    unmatched: [],
-  })
-})
-
-test('getIsFieldShowing for one criterion (eq)', () => {
-  const showIf: MatchFormFieldShowIfCondition = {
-    operator: 'OR',
-    criteria: [{ id: 0, operator: 'eq', value: true }],
-  }
-  const values: MatchFormValues = { 0: true, 1: undefined, 2: undefined }
-
-  expect(getIsFieldShowing(showIf, config.fields, values)).toEqual(true)
-})
-
-test('getIsFieldShowing for one criterion (gt)', () => {
-  const showIf: MatchFormFieldShowIfCondition = {
-    operator: 'OR',
-    criteria: [{ id: 2, operator: 'gt', value: 1 }],
-  }
-  const values: MatchFormValues = { 0: true, 1: undefined, 2: 2 }
-
-  expect(getIsFieldShowing(showIf, config.fields, values)).toEqual(true)
-})
-
-test('getIsFieldShowing for multiple criteria (OR)', () => {
-  const showIf: MatchFormFieldShowIfCondition = {
-    operator: 'OR',
-    criteria: [
-      { id: 0, operator: 'eq', value: true },
-      { id: 1, operator: 'eq', value: true },
-      { id: 2, operator: 'gt', value: 1 },
-    ],
-  }
-  const values1: MatchFormValues = { 0: true }
-  expect(getIsFieldShowing(showIf, config.fields, values1)).toEqual(true)
-
-  const values2: MatchFormValues = { 0: false, 1: true }
-  expect(getIsFieldShowing(showIf, config.fields, values2)).toEqual(true)
-
-  const values3: MatchFormValues = { 0: false, 1: false, 2: 2 }
-  expect(getIsFieldShowing(showIf, config.fields, values3)).toEqual(true)
-
-  const values4: MatchFormValues = { 0: false, 1: false, 2: 1 }
-  expect(getIsFieldShowing(showIf, config.fields, values4)).toEqual(false)
-})
-
-test('getIsFieldShowing for multiple criteria (AND)', () => {
-  const showIf: MatchFormFieldShowIfCondition = {
-    operator: 'AND',
-    criteria: [
-      { id: 0, operator: 'eq', value: true },
-      { id: 1, operator: 'eq', value: true },
-      { id: 2, operator: 'gt', value: 1 },
-    ],
-  }
-  const values1: MatchFormValues = { 0: true }
-  expect(getIsFieldShowing(showIf, config.fields, values1)).toEqual(false)
-
-  const values2: MatchFormValues = { 0: true, 1: false }
-  expect(getIsFieldShowing(showIf, config.fields, values2)).toEqual(false)
-
-  const values3: MatchFormValues = { 0: true, 1: true, 2: 2 }
-  expect(getIsFieldShowing(showIf, config.fields, values3)).toEqual(true)
-
-  const values4: MatchFormValues = { 0: true, 1: true, 2: 1 }
-  expect(getIsFieldShowing(showIf, config.fields, values4)).toEqual(false)
-})
-
-test('getFieldOptionLabelMap form config', () => {
-  const config: MatchFormConfig = {
-    groups: [],
-    fields: [
-      {
-        id: 0,
-        groupId: 0,
-        type: 'select',
-        name: 'foobar',
-        options: [
-          { value: 0, label: 'foo' },
-          { value: 1, label: 'bar' },
-        ],
-      },
-      {
-        id: 1,
-        groupId: 0,
-        type: 'number',
-        name: 'baz',
-      },
-      {
-        id: 2,
-        groupId: 0,
-        type: 'radio',
-        name: 'baz',
-        options: [{ value: 0, label: 'baz' }],
-      },
-    ],
-  }
-  expect(getFieldOptionLabelMap(config.fields)).toEqual({
-    0: { 0: 'foo', 1: 'bar' },
-    2: { 0: 'baz' },
+describe('getFieldOptionLabelMap', () => {
+  test('from form config', () => {
+    const config: MatchFormConfig = {
+      groups: [],
+      fields: [
+        {
+          id: 0,
+          groupId: 0,
+          type: 'select',
+          name: 'foobar',
+          options: [
+            { value: 0, label: 'foo' },
+            { value: 1, label: 'bar' },
+          ],
+        },
+        {
+          id: 1,
+          groupId: 0,
+          type: 'number',
+          name: 'baz',
+        },
+        {
+          id: 2,
+          groupId: 0,
+          type: 'radio',
+          name: 'baz',
+          options: [{ value: 0, label: 'baz' }],
+        },
+      ],
+    }
+    expect(getFieldOptionLabelMap(config.fields)).toEqual({
+      0: { 0: 'foo', 1: 'bar' },
+      2: { 0: 'baz' },
+    })
   })
 })
