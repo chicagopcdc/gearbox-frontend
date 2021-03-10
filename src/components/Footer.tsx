ec495012b6276500a512a729fd1066848afc4ba8
Bobae Kang <bkang.dev@gmail.com>
2021-03-10 10:39:43 -0600

    Modify footer logo spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import pedalLogo from '../assets/pedal-logo.svg'
 
 function Footer() {
   return (
-    <footer className="flex flex-wrap items-center justify-center lg:justify-between lg:text-left p-4 border-t border-solid border-primary">
+    <footer className="flex flex-wrap items-center justify-evenly lg:justify-between lg:text-left p-4 border-t border-solid border-primary">
       <a
         href="https://biologicalsciences.uchicago.edu/"
         className="m-2"
