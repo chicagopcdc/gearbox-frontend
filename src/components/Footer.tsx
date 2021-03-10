708d7db7949d430c59c8e9f57c6f387bd3757b7c
Bobae Kang <bkang.dev@gmail.com>
2021-03-10 10:38:57 -0600

    Add hyperlinks to footer logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,38 @@ import pedalLogo from '../assets/pedal-logo.svg'
 function Footer() {
   return (
     <footer className="flex flex-wrap items-center justify-center lg:justify-between lg:text-left p-4 border-t border-solid border-primary">
-      <img
-        src={uchicagoBSDlogo}
-        alt="University of Chicago Biological Sciences Division logo"
-        style={{ maxHeight: '80px' }}
-      />
-      <img
-        src={pcdcLogo}
-        alt="Pediatric Center Data Commons logo"
-        style={{ maxHeight: '80px' }}
-      />
-      <img src={pedalLogo} alt="PedAL logo" style={{ maxHeight: '80px' }} />
+      <a
+        href="https://biologicalsciences.uchicago.edu/"
+        className="m-2"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={uchicagoBSDlogo}
+          alt="University of Chicago Biological Sciences Division logo"
+          style={{ maxHeight: '80px' }}
+        />
+      </a>
+      <a
+        href="https://commons.cri.uchicago.edu/"
+        className="m-2"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={pcdcLogo}
+          alt="Pediatric Center Data Commons logo"
+          style={{ maxHeight: '80px' }}
+        />
+      </a>
+      <a
+        href="https://www.lls.org/childrens-initiative/pedal"
+        className="m-2"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={pedalLogo} alt="PedAL logo" style={{ maxHeight: '80px' }} />
+      </a>
     </footer>
   )
 }
