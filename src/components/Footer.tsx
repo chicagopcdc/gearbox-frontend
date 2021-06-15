e4cf6b8847f4caa769f1a95fa7d98e6d208eb76e
Bobae Kang <bkang.dev@gmail.com>
2021-06-15 09:13:06 -0500

    Update footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,69 @@
+import { Link } from 'react-router-dom'
 import uchicagoBSDlogo from '../assets/uchicago-BSD-logo.jpg'
 import pcdcLogo from '../assets/pcdc-logo.png'
 import pedalLogo from '../assets/pedal-logo.svg'
 
 function Footer() {
   return (
-    <footer className="flex flex-wrap items-center justify-evenly lg:justify-between lg:text-left p-4 border-t border-solid border-primary">
-      <a
-        href="https://biologicalsciences.uchicago.edu/"
-        className="m-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          src={uchicagoBSDlogo}
-          alt="University of Chicago Biological Sciences Division logo"
-          style={{ maxHeight: '80px' }}
-        />
-      </a>
-      <a
-        href="https://commons.cri.uchicago.edu/"
-        className="m-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          src={pcdcLogo}
-          alt="Pediatric Center Data Commons logo"
-          style={{ maxHeight: '80px' }}
-        />
-      </a>
-      <a
-        href="https://www.lls.org/childrens-initiative/pedal"
-        className="m-2"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={pedalLogo} alt="PedAL logo" style={{ maxHeight: '80px' }} />
-      </a>
+    <footer className="p-4 border-t border-solid border-primary">
+      <section className="flex flex-col md:flex-row items-center justify-between max-w-screen-lg mx-auto mb-12 md:mb-20">
+        <a
+          href="https://biologicalsciences.uchicago.edu/"
+          className="m-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={uchicagoBSDlogo}
+            alt="University of Chicago Biological Sciences Division logo"
+            style={{ maxHeight: '60px' }}
+          />
+        </a>
+        <a
+          href="https://commons.cri.uchicago.edu/"
+          className="m-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={pcdcLogo}
+            alt="Pediatric Center Data Commons logo"
+            style={{ maxHeight: '60px' }}
+          />
+        </a>
+        <a
+          href="https://www.lls.org/childrens-initiative/pedal"
+          className="m-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={pedalLogo} alt="PedAL logo" style={{ maxHeight: '60px' }} />
+        </a>
+      </section>
+      <section className="text-center text-sm">
+        <div className="mb-4">
+          <ul>
+            <li className="mx-2 inline underline">
+              <Link to="/about">About GEARBOx</Link>
+            </li>
+            •
+            <li className="mx-2 inline underline">
+              <Link to="/terms">Terms</Link>
+            </li>
+            •
+            <li className="mx-2 inline underline">
+              <a
+                href="https://commons.cri.uchicago.edu/contact/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>© 2021 Pediatric Cancer Data Commons</div>
+      </section>
     </footer>
   )
 }
