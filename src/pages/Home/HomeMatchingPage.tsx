8650a6e0d98d6543e03b2cd104e00a65557f51ac
Bobae Kang <bkang.dev@gmail.com>
2021-06-23 09:51:08 -0500

    Use tab view for matching page on small screen

--- src/pages/Home/HomeMatchingPage.tsx
+++ src/pages/Home/HomeMatchingPage.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import Button from '../../components/Inputs/Button'
 import MatchForm from '../../components/MatchForm'
 import MatchResult from '../../components/MatchResult'
+import useScreenSize from '../../hooks/useScreenSize'
 import {
   EligibilityCriterion,
   MatchCondition,
@@ -31,23 +33,56 @@ function HomeMatchingPage({
   const isMatchDataReady =
     config.fields !== undefined && Object.keys(userInput).length > 0
 
+  const screenSize = useScreenSize()
+  const [view, setView] = useState('form')
+
   return isMatchDataReady ? (
-    <div className="md:flex">
-      <section className="md:w-1/2 p-4 lg:px-8">
-        <h1 className="uppercase text-primary font-bold">
-          Patient Information
-        </h1>
-        <MatchForm {...{ config, userInput, updateUserInput, setIsUpdating }} />
-      </section>
-      <section
-        className={`md:w-1/2 p-4 lg:px-8 ${isUpdating ? 'bg-gray-100' : ''}`}
-      >
-        <h1 className="uppercase text-primary font-bold">Open Trials</h1>
-        <MatchResult
-          {...{ criteria, conditions, config, studies, userInput }}
-        />
-      </section>
-    </div>
+    <>
+      {screenSize.smAndDown && (
+        <div className="flex justify-center sticky top-0 bg-white">
+          <Button
+            size="small"
+            block
+            outline={view !== 'form'}
+            onClick={() => setView('form')}
+          >
+            Patient Information
+          </Button>
+          <Button
+            size="small"
+            block
+            outline={view !== 'result'}
+            onClick={() => setView('result')}
+          >
+            Open Trials
+          </Button>
+        </div>
+      )}
+      <div className="md:flex">
+        {(screenSize.mdAndUp || view === 'form') && (
+          <section className="md:w-1/2 p-4 lg:px-8">
+            <h1 className="uppercase text-primary font-bold">
+              Patient Information
+            </h1>
+            <MatchForm
+              {...{ config, userInput, updateUserInput, setIsUpdating }}
+            />
+          </section>
+        )}
+        {(screenSize.mdAndUp || view === 'result') && (
+          <section
+            className={`md:w-1/2 p-4 lg:px-8 ${
+              isUpdating ? 'bg-gray-100' : ''
+            }`}
+          >
+            <h1 className="uppercase text-primary font-bold">Open Trials</h1>
+            <MatchResult
+              {...{ criteria, conditions, config, studies, userInput }}
+            />
+          </section>
+        )}
+      </div>
+    </>
   ) : (
     <div>Loading...</div>
   )
