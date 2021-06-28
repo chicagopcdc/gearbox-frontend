e1fdaa88600f565927a4b01c220a2252d851d334
Bobae Kang <bkang.dev@gmail.com>
2021-06-28 14:55:50 -0500

    Redesign login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,23 @@ import gearboxLogo from '../assets/gearbox-logo.svg'
 
 function Login() {
   return (
-    <div className="flex flex-col h-full align-center justify-center max-w-sm mx-auto">
-      <img src={gearboxLogo} alt="GEARBOx logo" className="mb-12" />
+    <div className="flex h-full items-center justify-center">
+      <div className="border border-gray border-solid px-4 sm:px-8 pt-12 pb-20 sm:pb-28">
+        <div className="flex justify-center mb-4">
+          <img
+            src={gearboxLogo}
+            alt="GEARBOx logo"
+            style={{ height: '40px' }}
+          />
+        </div>
+        <h1 className="mb-16 text-lg text-center">Log in to your account</h1>
 
-      <div className="mb-4 mx-12">
-        <GoogleLoginButton onClick={initFenceOAuth} />
+        <div className="mb-4">
+          <GoogleLoginButton onClick={initFenceOAuth} />
+        </div>
+        <p className="text-sm">
+          If this is your first time, you will be asked to register.
+        </p>
       </div>
     </div>
   )
