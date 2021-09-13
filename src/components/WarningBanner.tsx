b0b90e75005825a5e8e3fa8916e6220e2067c428
Brian Furner <bfurner@bsd.uchicago.edu>
2021-09-13 10:57:24 -0500

    updating warning banner verbiage for the pilot

--- src/components/WarningBanner.tsx
+++ src/components/WarningBanner.tsx
@@ -4,7 +4,8 @@ function WarningBanner() {
   return (
     <div className="bg-primary font-bold text-sm text-white text-center p-2">
       <img className="mr-2 inline" src={WarningIcon} alt="warning icon" /> This
-      site is a prototype created for demo purposes only.
+      site is intended for pilot use only at this time and matching results
+      should not be used for eligibility assessment of actual patients.
     </div>
   )
 }
