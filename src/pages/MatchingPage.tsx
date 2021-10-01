7d17ca30a3a596a30976b5a716b54a95f10ee9e5
Bobae Kang <bkang.dev@gmail.com>
2021-10-01 09:29:18 -0500

    Return early if still loading match data

--- src/pages/MatchingPage.tsx
+++ src/pages/MatchingPage.tsx
@@ -30,83 +30,78 @@ function MatchingPage({
   updateMatchInput,
 }: MatchingPageProps) {
   const [isUpdating, setIsUpdating] = useState(false)
-
-  const isMatchDataReady =
-    config.fields !== undefined && Object.keys(matchInput).length > 0
-
   const screenSize = useScreenSize()
   const [view, setView] = useState<'form' | 'result'>('form')
+
+  if (config.fields === undefined || Object.keys(matchInput).length === 0)
+    return <div>Loading...</div>
 
   const matchDetails = getMatchDetails(criteria, conditions, config, matchInput)
   const matchGroups = getMatchGroups(matchDetails)
 
-  return isMatchDataReady ? (
-    screenSize.smAndDown ? (
-      <>
-        <div
-          className="flex justify-center sticky top-0 bg-white z-10"
-          style={{
-            minHeight: '2.5rem',
-          }}
+  return screenSize.smAndDown ? (
+    <>
+      <div
+        className="flex justify-center sticky top-0 bg-white z-10"
+        style={{
+          minHeight: '2.5rem',
+        }}
+      >
+        <Button
+          size="small"
+          block
+          outline={view !== 'form'}
+          onClick={() => setView('form')}
         >
-          <Button
-            size="small"
-            block
-            outline={view !== 'form'}
-            onClick={() => setView('form')}
-          >
-            Patient Information
-          </Button>
-          <Button
-            size="small"
-            block
-            outline={view !== 'result'}
-            onClick={() => setView('result')}
-          >
-            Open Trials
-          </Button>
-        </div>
-        <section className={`p-4 lg:px-8 ${view === 'form' ? '' : 'hidden'} `}>
+          Patient Information
+        </Button>
+        <Button
+          size="small"
+          block
+          outline={view !== 'result'}
+          onClick={() => setView('result')}
+        >
+          Open Trials
+        </Button>
+      </div>
+      <section className={`p-4 lg:px-8 ${view === 'form' ? '' : 'hidden'} `}>
+        <MatchForm
+          {...{ config, matchInput, updateMatchInput, setIsUpdating }}
+        />
+      </section>
+      <section
+        className={`p-4 lg:px-8 transition-colors duration-300 ${
+          isUpdating ? 'bg-gray-100' : 'bg-white'
+        } ${view === 'result' ? '' : 'hidden'} `}
+      >
+        <MatchResult {...{ matchDetails, matchGroups, studies }} />
+      </section>
+    </>
+  ) : (
+    <div className="flex h-screen pb-8">
+      <section className="h-full overflow-scroll w-1/2">
+        <h1 className="sticky top-0 bg-white uppercase text-primary font-bold pl-4 lg:pl-8 py-2 z-10">
+          Patient Information
+        </h1>
+        <div className="px-4 lg:px-8 pb-4">
           <MatchForm
             {...{ config, matchInput, updateMatchInput, setIsUpdating }}
           />
-        </section>
-        <section
-          className={`p-4 lg:px-8 transition-colors duration-300 ${
+        </div>
+      </section>
+      <section className="h-full overflow-scroll w-1/2">
+        <h1 className="sticky top-0 bg-white uppercase text-primary font-bold pl-4 lg:pl-8 py-2 z-10">
+          Open Trials
+        </h1>
+        <div
+          className={`px-4 lg:px-8 pb-4 transition-colors duration-300 ${
             isUpdating ? 'bg-gray-100' : 'bg-white'
-          } ${view === 'result' ? '' : 'hidden'} `}
+          }`}
         >
           <MatchResult {...{ matchDetails, matchGroups, studies }} />
-        </section>
-      </>
-    ) : (
-      <div className="flex h-screen pb-8">
-        <section className="h-full overflow-scroll w-1/2">
-          <h1 className="sticky top-0 bg-white uppercase text-primary font-bold pl-4 lg:pl-8 py-2 z-10">
-            Patient Information
-          </h1>
-          <div className="px-4 lg:px-8 pb-4">
-            <MatchForm
-              {...{ config, matchInput, updateMatchInput, setIsUpdating }}
-            />
-          </div>
-        </section>
-        <section className="h-full overflow-scroll w-1/2">
-          <h1 className="sticky top-0 bg-white uppercase text-primary font-bold pl-4 lg:pl-8 py-2 z-10">
-            Open Trials
-          </h1>
-          <div
-            className={`px-4 lg:px-8 pb-4 transition-colors duration-300 ${
-              isUpdating ? 'bg-gray-100' : 'bg-white'
-            }`}
-          >
-            <MatchResult {...{ matchDetails, matchGroups, studies }} />
-          </div>
-        </section>
-      </div>
-    )
-  ) : (
-    <div>Loading...</div>
+        </div>
+      </section>
+    </div>
   )
 }
 
