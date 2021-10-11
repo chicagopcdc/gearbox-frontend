e24d9e5c49ba6c4017f0731807d62379349adf59
Bobae Kang <bkang.dev@gmail.com>
2021-10-11 15:43:40 -0500

    Add match form menu UI elements

--- src/pages/MatchingPage.tsx
+++ src/pages/MatchingPage.tsx
@@ -1,4 +1,11 @@
 import { useState } from 'react'
+import {
+  MoreHorizontal,
+  ToggleLeft,
+  ToggleRight,
+  RotateCcw,
+} from 'react-feather'
+import ReactTooltip from 'react-tooltip'
 import Button from '../components/Inputs/Button'
 import MatchForm from '../components/MatchForm'
 import MatchResult from '../components/MatchResult'
@@ -36,6 +43,18 @@ function MatchingPage({
   const [isUpdating, setIsUpdating] = useState(false)
   const screenSize = useScreenSize()
   const [view, setView] = useState<'form' | 'result'>('form')
+  const [showFormOptions, setShowFormOptions] = useState(false)
+  const [isFilterActive, setIsFilterActive] = useState(false)
+  function toggleFormOptions() {
+    setShowFormOptions((show) => !show)
+  }
+  function toggleFilter() {
+    setIsFilterActive((isActive) => !isActive)
+    console.log('toggle filter')
+  }
+  function handleReset() {
+    console.log('reset')
+  }
 
   if (config.fields === undefined || Object.keys(matchInput).length === 0)
     return <div>Loading...</div>
@@ -64,7 +83,73 @@ function MatchingPage({
           outline={view !== 'form'}
           onClick={() => setView('form')}
         >
-          Patient Information
+          <div className="w-full relative py-1">
+            Patient Info
+            {view === 'form' && (
+              <div className="normal-case">
+                <button
+                  className={`ml-2 p-1 absolute right-0 top-0 ${
+                    showFormOptions ? 'bg-red-500' : 'hover:bg-red-500'
+                  }`}
+                  data-for="match-form-menu"
+                  data-tip
+                  onClick={toggleFormOptions}
+                >
+                  <MoreHorizontal className="inline" size="1rem" />
+                  <ReactTooltip
+                    border
+                    borderColor="black"
+                    id="match-form-menu"
+                    effect="solid"
+                    place="bottom"
+                    type="light"
+                  >
+                    <span>Options</span>
+                  </ReactTooltip>
+                </button>
+                {showFormOptions && (
+                  <div className="absolute right-0 origin-top-right w-44 bg-white border border-gray-300 shadow-md mt-2 p-1">
+                    <ul className="w-full text-sm text-center text-primary">
+                      <li className="hover:bg-red-100">
+                        <button
+                          className="w-full p-2"
+                          onClick={toggleFilter}
+                          data-tip
+                          data-for="match-form-filter"
+                        >
+                          {isFilterActive ? (
+                            <ToggleRight className="inline text" />
+                          ) : (
+                            <ToggleLeft className="inline text-gray-500" />
+                          )}
+                          <span className="mx-2">Filter questions</span>
+                        </button>
+                        <ReactTooltip
+                          id="match-form-filter"
+                          border
+                          borderColor="black"
+                          effect="solid"
+                          place="right"
+                          type="light"
+                        >
+                          <div style={{ maxWidth: '100px' }}>
+                            Filter to display the relevant questions only or see
+                            all
+                          </div>
+                        </ReactTooltip>
+                      </li>
+                      <li className="hover:bg-red-100">
+                        <button className="w-full p-2" onClick={handleReset}>
+                          <RotateCcw className="inline mr-2" size="1rem" />
+                          Reset
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </Button>
         <Button
           size="small"
@@ -96,8 +181,69 @@ function MatchingPage({
   ) : (
     <div className="flex h-screen pb-8">
       <section className="h-full overflow-scroll w-1/2">
-        <h1 className="sticky top-0 bg-white uppercase text-primary font-bold pl-4 lg:pl-8 py-2 z-10">
-          Patient Information
+        <h1 className="sticky top-0 bg-white uppercase text-primary font-bold px-4 lg:px-8 py-2 z-10 flex items-end justify-between">
+          <span>Patient Information</span>
+          <div className="inline relative font-normal normal-case text-base">
+            <button
+              className={`px-2 py-1 ${
+                showFormOptions ? 'bg-red-100' : 'hover:bg-red-100'
+              }`}
+              data-for="match-form-menu"
+              data-tip
+              onClick={toggleFormOptions}
+            >
+              <MoreHorizontal className="inline" size="1rem" />
+              <ReactTooltip
+                border
+                borderColor="black"
+                id="match-form-menu"
+                effect="solid"
+                place="left"
+                type="light"
+              >
+                <span>Options</span>
+              </ReactTooltip>
+            </button>
+            {showFormOptions && (
+              <div className="absolute right-0 origin-top-right w-44 bg-white border border-gray-300 shadow-md mt-2 p-1">
+                <ul className="w-full text-sm text-center">
+                  <li className="hover:bg-red-100">
+                    <button
+                      className="w-full p-2"
+                      data-for="match-form-filter"
+                      data-tip
+                      onClick={toggleFilter}
+                    >
+                      {isFilterActive ? (
+                        <ToggleRight className="inline text" />
+                      ) : (
+                        <ToggleLeft className="inline text-gray-500" />
+                      )}
+                      <span className="mx-2">Filter questions</span>
+                    </button>
+                    <ReactTooltip
+                      border
+                      borderColor="black"
+                      id="match-form-filter"
+                      effect="solid"
+                      place="left"
+                      type="light"
+                    >
+                      <div style={{ maxWidth: '200px' }}>
+                        Filter to display the relevant questions only or see all
+                      </div>
+                    </ReactTooltip>
+                  </li>
+                  <li className="hover:bg-red-100">
+                    <button className="w-full p-2" onClick={handleReset}>
+                      <RotateCcw className="inline mr-2" size="1rem" />
+                      Reset
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </h1>
         <div className="px-4 lg:px-8 pb-4">
           <MatchForm
