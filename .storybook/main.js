e9b7a799b799917df32a0624c50b9060fc295f98
Bobae Kang <bkang.dev@gmail.com>
2022-05-06 11:50:20 -0500

    Add storybook controls addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-controls',
   ],
   core: {
     builder: 'webpack5',
