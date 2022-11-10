45364c10a7606b8a63a3d48f2706e4d2269f7657
Bobae Kang <bkang.dev@gmail.com>
2022-11-10 17:43:38 -0600

    Improve matching page error message
    Add more steps to try if "try again" button fails

--- src/pages/MatchingPage.tsx
+++ src/pages/MatchingPage.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-feather'
 import ReactTooltip from 'react-tooltip'
 import Button from '../components/Inputs/Button'
+import LinkExternal from '../components/LinkExternal'
 import MatchForm from '../components/MatchForm'
 import MatchResult from '../components/MatchResult'
 import type useGearboxData from '../hooks/useGearboxData'
@@ -35,10 +36,37 @@ function MatchingPage({ action, state, status }: MatchingPageProps) {
   if (status === 'error')
     return (
       <>
-        <div className="pb-4">Something went wrong!</div>
-        <Button size="normal" onClick={fetchAll}>
-          Try again
-        </Button>
+        <div className="pb-8">
+          <p className="pb-4">Something went wrong! Please try again.</p>
+          <Button size="normal" onClick={fetchAll}>
+            Try again
+          </Button>
+        </div>
+        <div className="pb-4">
+          <p>
+            &quot;Try again&quot; button did not fix the problem? Please try
+            hard refresh.
+          </p>
+          <ul className="list-disc pl-8">
+            <li>
+              Chrome or Firefox: Press <strong>Ctrl + F5</strong> on Windows or{' '}
+              <strong>Cmd + Shift + R</strong> on Mac
+            </li>
+            <li>
+              Safari (Mac): Press <strong>Cmd + Option + R</strong>
+            </li>
+          </ul>
+        </div>
+        <p>
+          Hard refresh did not fix the problem? Please reach out to{' '}
+          <LinkExternal
+            className="underline text-primary"
+            to="mailto:gearbox_help@lists.uchicago.edu"
+          >
+            gearbox_help@lists.uchicago.edu
+          </LinkExternal>
+          .
+        </p>
       </>
     )
 
