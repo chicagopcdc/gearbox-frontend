d7b4d2fd46a58bc49f6dafd899bc57fce4e1bedb
Luca Graglia <graglia01@gmail.com>
2023-02-17 09:31:20 -0600

    add bucket url to connect-src with sed

--- dockerStart.sh
+++ dockerStart.sh
@@ -4,4 +4,8 @@
 #
 set -eu
 
+# add https://gearbox-dev-data-bucket-with-versioning.s3.amazonaws.com after connect-src in content security policy
+s3_bucket="https://gearbox-dev-data-bucket-with-versioning.s3.amazonaws.com"
+sed -i -e "s|connect-src|& $s3_bucket|" /usr/share/nginx/html/index.html
+
 /usr/sbin/nginx -g 'daemon off;'
