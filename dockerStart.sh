eb683e3fb5c6afa444f94d96c819b48105baa728
Luca Graglia <graglia01@gmail.com>
2023-02-17 11:35:18 -0600

    get value from env variable

--- dockerStart.sh
+++ dockerStart.sh
@@ -5,7 +5,6 @@
 set -eu
 
 # add https://gearbox-dev-data-bucket-with-versioning.s3.amazonaws.com after connect-src in content security policy
-s3_bucket="https://gearbox-dev-data-bucket-with-versioning.s3.amazonaws.com"
-sed -i -e "s|connect-src|& $s3_bucket|" /usr/share/nginx/html/index.html
+sed -i -e "s|connect-src|& $GEARBOX_S3_BUCKET|" /usr/share/nginx/html/index.html
 
 /usr/sbin/nginx -g 'daemon off;'
