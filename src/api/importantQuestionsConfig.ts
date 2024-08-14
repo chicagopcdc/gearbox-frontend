ba9b347cd00dfa99aa55bb08325f6777d3637037
Janet Jiang <janetjiang1103@gmail.com>
2024-08-13 22:09:32 -0700

    fetch url from the backend

--- src/api/importantQuestionsConfig.ts
+++ src/api/importantQuestionsConfig.ts
@@ -2,7 +2,8 @@ import { ImportantQuestionConfig } from '../model'
 import { fetchGearbox } from './utils'
 
 export function getImportantQuestionsConfig() {
-  return fetchGearbox(
-    'https://test-compose-gearbox-data-bucket-with-versioning.s3.amazonaws.com/important_questions.json?AWSAccessKeyId=&Expires=1721249859'
-  ).then((res) => res.json() as Promise<ImportantQuestionConfig>)
+  return fetchGearbox('/gearbox/important-questions')
+    .then((res) => res.json())
+    .then(fetch)
+    .then((res) => res.json() as Promise<ImportantQuestionConfig>)
 }
